5f9ebcdcc1f8367770bb3acda7ad11d2b1f1603d
Zaine <zaine.ancheta@ucalgary.ca>
2026-01-22 10:35:02 -0700

    Init

---
+++ assign.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int search () {
+
+}
+
+
+int main() {
+
+    // Initialize table
+    int table [100][100];
+
+    int i, j;
+
+    for(i = 0; i<100; i++) {
+
+        for(j = 0; j<100; j++) {
+            table[i][j] = 0;
+        }
+
+    }
+
+}
+
